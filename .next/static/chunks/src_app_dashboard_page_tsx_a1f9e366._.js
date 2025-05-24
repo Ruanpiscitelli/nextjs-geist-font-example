a3f448b924a7e537d17ad14230a43f5e2b352424
Ruanpiscitelli <piscitelliruan@gmail.com>
2025-05-24 00:48:02 -0300

    Add .next/static/chunks/src_app_dashboard_page_tsx_a1f9e366._.js 1748058481950

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_a1f9e366._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_c5808c23._.js",
+  "static/chunks/src_2bf9f362._.js"
+],
+    source: "dynamic"
+});
